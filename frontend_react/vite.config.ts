76303818ed7df32ff5b92de6b556b3f40633929a
5fyt <2977366691@qq.com>
2024-01-19 16:22:17 +0800

    'feat:windicss和vite.config.ts配置'

--- frontend_react/vite.config.ts
+++ frontend_react/vite.config.ts
@@ -1,7 +1,65 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy';
+import path from 'path';
+import { type ConfigEnv, type UserConfig, loadEnv } from 'vite';
+
+import WindiCSS from 'vite-plugin-windicss';
+
+const CWD = process.cwd();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  // 环境变量
+  const { VITE_BASE_URL, VITE_DROP_CONSOLE } = loadEnv(mode, CWD);
+
+  const isBuild = command === 'build';
+  console.log('当前执行环境：', isBuild);
+  return {
+    base: VITE_BASE_URL,
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: path.resolve(__dirname, 'src')
+        },
+        {
+          find: '~antd',
+          replacement: path.resolve(__dirname, 'node_modules/antd')
+        }
+      ]
+    },
+    // server: {
+    //   port: 8889,
+    //   proxy: {
+    //     '/api': {
+    //       target: `http://175.24.200.3:7001`,
+    //       // changeOrigin: true,
+    //       rewrite: path => path.replace(/^\/api/, '')
+    //     }
+    //   }
+    // },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          modifyVars: { '@primary-color': '#13c2c2' }
+        }
+        // ....
+      }
+    },
+    plugins: [
+      WindiCSS(),
+      legacy({
+        targets: ['ie >= 11'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+      })
+    ],
+    build: {
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          drop_console: !!VITE_DROP_CONSOLE
+        }
+      }
+    }
+  };
+};
